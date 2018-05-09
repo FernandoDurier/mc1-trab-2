3b1acc100e176022adbc1b930576ace4eab25f54
Rafael Vieira <rvieira.research@gmail.com>
2018-05-09 16:08:37 -0300

    Random sampling and calculating precision, recall and f-measure.

--- src/cli.py
+++ src/cli.py
@@ -2,6 +2,7 @@ import click
 import csv
 
 from preprocess import clean, p_method, n_method
+from evaluate import sample, metrics, correct, hypothesis_tests
 from tests import kolgomorov2samples, testz
 
 @click.group()
@@ -10,16 +11,35 @@ def cli():
 
 @cli.command()
 @click.argument('input_filename', type=click.Path(exists=True))
-def preprocess(input_filename):
+@click.argument('gs_filename', type=click.Path(exists=True))
+def process(input_filename, gs_filename):
     dataset = []
     with open(input_filename) as f:
         reader = csv.reader(f)
         dataset = [row for row in reader]
 
     dataset = clean(dataset)
-    print("Result of N_Method:",n_method(dataset, 5))
-    print("Result of P_Method:",p_method(dataset, 5))
-    print("Testing the n method against the p method:",kolgomorov2samples(n_method(dataset, 5),p_method(dataset, 5)))
-    print("Test Z between methods n and p:",testz(n_method(dataset, 5),p_method(dataset, 5)))
+    sampled_dataset = sample(dataset, 20)
+    #print("Result of N_Method:",n_method(sampled_dataset, 5))
+    #print("Result of P_Method:",p_method(sampled_dataset, 5))
+    #print("Testing the n method against the p method:",kolgomorov2samples(n_method(sampled_dataset, 5),p_method(sampled_dataset, 5)))
+    #print("Test Z between methods n and p:",testz(n_method(sampled_dataset, 5),p_method(sampled_dataset, 5)))
+
+    gs = []
+    with open(gs_filename) as f:
+        reader = csv.reader(f)
+        gs = [row for row in reader]
+
+    y_true = correct(clean(gs), 3)
+    
+
+    for sample_size in range(20, 80):
+        p, r, f = metrics(dataset, y_true, 20)
+        print(f"Sample size = {sample_size}")
+        print("Results for precision:", hypothesis_tests(p))
+        print("Results for recall:", hypothesis_tests(r))
+        print("Results for f1_score:", hypothesis_tests(f))
+
+
 if __name__ == "__main__":
     cli()
---
+++ src/evaluate.py
@@ -0,0 +1,55 @@
+import numpy as np
+from sklearn.metrics import precision_recall_fscore_support
+from preprocess import p_method, n_method
+from tests import kolgomorov2samples, testz
+
+
+def sample(dataset, size):
+    samples_idx = np.random.choice(dataset.shape[0], size=size, replace=False)
+    return dataset[samples_idx, :]
+
+def correct(gs, mean):
+    gs = gs.astype(np.float64)
+    indexes = (gs[:, 1] > mean).A1
+    y_true = np.zeros(42).astype(np.int)
+    for index, correct in enumerate((gs[:, 1] > mean).A1):
+        if correct:
+            y_true[index] = 1
+
+    return y_true
+
+def predict(proposals):
+    return [1 if i in proposals else 0 for i in range(0, 42)]
+
+def metrics(dataset, y_true, sample_size, runs=100):
+    precision = np.zeros((runs, 3))
+    recall = np.zeros((runs, 3))
+    f_score = np.zeros((runs, 3))
+    for run in range(0, runs):
+        sampled_dataset = sample(dataset, sample_size)
+        p_n, r_n, f_n, _ = precision_recall_fscore_support(
+            y_true,
+            predict(n_method(sampled_dataset, 21)), # 21 is 0.5 * 42 (compression ratio * #proposals)
+            average='macro',
+        )
+        p_p, r_p, f_p, _ = precision_recall_fscore_support(
+            y_true,
+            predict(p_method(sampled_dataset, 21)), # 21 is 0.5 * 42 (compression ratio * #proposals)
+            average='macro',
+        )
+        precision[run] = np.array([run, p_n, p_p])
+        recall[run] = np.array([run, r_n, r_p])
+        f_score[run] = np.array([run, f_n, f_p])
+
+    return precision, recall, f_score 
+
+
+def hypothesis_tests(metric_matrix):
+    nonparametric = kolgomorov2samples(metric_matrix[:, 1], metric_matrix[:, 2])
+    parametric = testz(metric_matrix[:, 1], metric_matrix[:, 2])
+    return np.array([
+        nonparametric.statistic,
+        nonparametric.pvalue,
+        parametric[0],
+        parametric[1],
+    ])
